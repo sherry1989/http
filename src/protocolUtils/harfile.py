a2c0b14c1cab9039a60d595eb94c46e55b2ede95
sherry1989 <sherry.wang1989@gmail.com>
2012-11-27 17:44:32 +0800

    copy from SPDY4_header_compression_example and modify them: 1. modify MakeDefaultHeaders, not ignore any header and not change key name to lower. 2. change ReadHarFile to ReadHarFileForHttp, not ignore any header.

---
+++ src/protocolUtils/harfile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import re
+
+def MakeDefaultHeaders(list_o_dicts):
+  retval = {}
+  for kvdict in list_o_dicts:
+    key = kvdict["name"]
+    val = kvdict["value"]
+    if key == "host":
+      key = ":host"
+    if key in retval:
+      retval[key] = retval[key] + '\0' + val
+    else:
+      retval[key] = val
+  return retval
+
+def ReadHarFileForHttp(filename):
+  f = open(filename)
+  null = None
+  true = 1
+  false = 0
+  s = f.read()
+  o = eval(s)
+  request_headers = []
+  response_headers = []
+  for entry in o["log"]["entries"]:
+    request = entry["request"]
+    header = MakeDefaultHeaders(request["headers"])
+    header[":method"] = request["method"].lower()
+    header[":path"] = re.sub("^[^:]*://[^/]*/","/", request["url"])
+    header[":version"] = re.sub("^[^/]*/","", request["httpVersion"])
+    header[":scheme"] = re.sub("^([^:]*):.*$", '\\1', request["url"]).lower()
+    if not ":host" in request_headers:
+      header[":host"] = re.sub("^[^:]*://([^/]*)/.*$","\\1", request["url"])
+    if not header[":scheme"] in ["http", "https"]:
+      continue
+    request_headers.append(header)
+
+    response = entry["response"]
+    header = MakeDefaultHeaders(response["headers"])
+    header[":status"] = re.sub("^([0-9]*).*","\\1", str(response["status"]))
+    header[":status-text"] = response["statusText"]
+    header[":version"] = re.sub("^[^/]*/","", response["httpVersion"])
+    response_headers.append(header)
+  return (request_headers, response_headers)
+
+
---
+++ src/protocolUtils/harfile_translator.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from harfile import ReadHarFileForHttp
+from optparse import OptionParser
+import sys
+import os
+
+def FormatIt(frame):
+  for (k, v) in frame.iteritems():
+    print "%s: %s" %(k, v)
+
+def main():
+  parser = OptionParser()
+  (options, args) = parser.parse_args()
+  if not args:
+    return
+  if args >= 1:
+    requests = []
+    responses = []
+    for filename in args:
+      sys.stderr.write(filename)
+      (har_requests, har_responses) = ReadHarFileForHttp(filename)
+      requests.extend(har_requests)
+      responses.extend(har_responses)
+  for i in xrange(len(requests)):
+    FormatIt(requests[i])
+    print
+    FormatIt(responses[i])
+    print
+  sys.stdin.close()
+  sys.stdout.close()
+  sys.stderr.close()
+  os.close(0)
+  os.close(1)
+  os.close(2)
+
+main()
